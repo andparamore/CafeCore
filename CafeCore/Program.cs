6c649ea4eca50dcf97de86f7bab13ee8055fe6e3
andparamore <Andrey.Paramore@yandex.ru>
2022-11-24 14:28:38 +0300

    test commit stop

--- CafeCore/Program.cs
+++ CafeCore/Program.cs
@@ -9,7 +9,7 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
         
         // Add services to the container.
-        
+
 
         builder.Services.AddControllers();
         if (builder.Environment.IsDevelopment())
