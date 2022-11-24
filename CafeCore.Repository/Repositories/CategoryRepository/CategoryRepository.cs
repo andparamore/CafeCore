048c67a99af7338d530c5ab33010dbee082dddec
andparamore <Andrey.Paramore@yandex.ru>
2022-11-24 17:46:53 +0300

    add categoryRepo

--- CafeCore.Repository/Repositories/CategoryRepository/CategoryRepository.cs
+++ CafeCore.Repository/Repositories/CategoryRepository/CategoryRepository.cs
@@ -1,5 +1,21 @@
-﻿namespace CafeCore.Repositories.Repositories.CategoryRepository;
+﻿using CafeCore.Models.Models;
+
+namespace CafeCore.Repositories.Repositories.CategoryRepository;
 
 public class CategoryRepository : ICategoryRepository
 {
+    public Task<int> CreateCategory(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> GetCategoryByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
